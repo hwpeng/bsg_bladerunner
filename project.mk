da6cc97df36f2853374ce176b0ec222e532b875f
Huwan Peng <hwpeng@uw.edu>
2019-11-19 11:30:48 -0800

    Replace one core to drlp. DRLP config w/r passed. DRAM w/r passed. DRLP-DRAM w/r passed.

--- project.mk
+++ project.mk
@@ -25,7 +25,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-DEPENDENCIES           := bsg_manycore bsg_f1 basejump_stl
+DEPENDENCIES           := bsg_manycore bsg_f1 basejump_stl drlp
 
 BLADERUNNER_ROOT       := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 BUILD_PATH             := $(BLADERUNNER_ROOT)
@@ -36,6 +36,8 @@ BSG_MANYCORE_DIR       := $(BLADERUNNER_ROOT)/bsg_manycore
 BSG_MANYCORE_COMMIT_ID := $(shell cd $(BSG_MANYCORE_DIR); git rev-parse --short HEAD)
 BASEJUMP_STL_DIR       := $(BLADERUNNER_ROOT)/basejump_stl
 BASEJUMP_STL_COMMIT_ID := $(shell cd $(BASEJUMP_STL_DIR); git rev-parse --short HEAD)
+DRLP_DIR               := $(BLADERUNNER_ROOT)drlp
+DRLP_COMMIT_ID         := $(shell cd $(DRLP_DIR); git rev-parse --short HEAD)
 
 FPGA_IMAGE_VERSION     := 3.4.1
 AWS_FPGA_VERSION       := v1.4.5
